79cd969407cf2a5657d671546a04ab6cb53cd1c6
leejeeh7 <54521573+leejeeh8411@users.noreply.github.com>
2023-07-07 13:16:57 +0900

    first commit

---
+++ Master/Plc.cpp
@@ -0,0 +1,118 @@
+#include "stdafx.h"
+#include "Plc.h"
+
+
+CPlc::CPlc()
+{
+	Init();
+}
+
+
+CPlc::~CPlc()
+{
+}
+
+void CPlc::Init()
+{
+	/***************************************************/
+	/*  Initialize OLE Library                         */
+	if (!AfxOleInit())
+	{
+		//로그로 적자
+		//AfxMessageBox(_T("AfxOleInit() failed."));
+		//return FALSE;
+	}
+	/***************************************************/
+
+	/* ACT Compornent Instance Create				   */
+	HRESULT hr;
+	
+
+#ifdef _WIN64
+	hr = CoCreateInstance(CLSID_ActUtlType64,
+		NULL,
+		CLSCTX_LOCAL_SERVER,
+		IID_IActUtlType64,
+		(LPVOID*)&m_plc);
+	if (!SUCCEEDED(hr))
+	{
+		AfxMessageBox(_T("ActUtlType64 CoCreateInstance() failed."));
+		//EndDialog(IDOK);
+		m_bCreatePlcInstance = false;
+	}
+#else
+	hr = CoCreateInstance(CLSID_ActUtlType,
+		NULL,
+		CLSCTX_INPROC_SERVER,
+		IID_IActUtlType,
+		(LPVOID*)&m_plc);
+	if (!SUCCEEDED(hr))
+	{
+		AfxMessageBox(_T("ActUtlType CoCreateInstance() failed."));
+		//EndDialog(IDOK);
+		m_bCreatePlcInstance = false;
+	}
+#endif
+
+	
+}
+
+bool CPlc::Open(int nLogicalNum)
+{
+	bool bSuccess = false;
+	if (m_bCreatePlcInstance == true) {
+		HRESULT hr;
+		
+		long	lRet;
+		m_plc->put_ActLogicalStationNumber(nLogicalNum);
+		hr = m_plc->Open(&lRet);
+
+		if (SUCCEEDED(hr)){
+			bSuccess = true;
+		}
+		else {
+			bSuccess = false;
+		}
+	}
+	return bSuccess;
+}
+
+bool CPlc::ReadBlock(CString strAddress,  int nSize, short* shValue)
+{
+	bool bSuccess = false;
+	HRESULT hr;
+	long	lRet;
+	BSTR	sAddress = strAddress.AllocSysString();
+
+	hr = m_plc->ReadDeviceBlock2(sAddress, nSize, shValue, &lRet);
+
+	if (SUCCEEDED(hr)) {
+		if (lRet == 0x00) {
+			bSuccess = true;
+		}
+	}
+	else {
+		bSuccess = false;
+	}
+	return bSuccess;
+}
+
+bool CPlc::WriteBlock(CString strAddress, int nSize, short* shValue)
+{
+	bool bSuccess = false;
+	HRESULT hr;
+	long	lRet;
+	BSTR	sAddress = strAddress.AllocSysString();
+
+	hr = m_plc->WriteDeviceBlock2(sAddress, nSize, shValue, &lRet);
+
+	if (SUCCEEDED(hr)) {
+		if (lRet == 0x00) {
+			bSuccess = true;
+		}
+	}
+	else {
+		bSuccess = false;
+	}
+	return bSuccess;
+}
---
+++ Master/Plc.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#ifdef _WIN64
+#include "ActUtlType64_i.h"
+#include "ActUtlType64_i.c"
+#else
+#include "ActUtlType_i.h"
+#include "ActUtlType_i.c"
+#endif
+
+class CPlc
+{
+public:
+	CPlc();
+	~CPlc();
+
+
+	bool CPlc::Open(int nLogicalNum);
+	bool CPlc::ReadBlock(CString strAddress, int nSize, short* shValue);
+	bool CPlc::WriteBlock(CString strAddress, int nSize, short* shValue);
+
+private:
+#ifdef _WIN64
+	IActUtlType64* m_plc;
+#else
+	IActUtlType* m_plc;
+#endif
+
+	bool m_bCreatePlcInstance = true;
+	void CPlc::Init();
+
+};
+
